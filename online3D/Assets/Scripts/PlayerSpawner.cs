55fbda4dd670b72a63dbaa6518d885d927f4ba7f
juliee <167346442+jujuhkn@users.noreply.github.com>
2026-03-18 10:55:12 -0300

    uq

--- online3D/Assets/Scripts/PlayerSpawner.cs
+++ online3D/Assets/Scripts/PlayerSpawner.cs
@@ -1,7 +1,17 @@
+using Fusion;
 using UnityEngine;
 
-public class PlayerSpawner : MonoBehaviour
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
+    [SerializeField] private GameObject _playerPrefab;
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(_playerPrefab, new Vector3(0, 1, 0), Quaternion.identity, Runner.LocalPlayer);
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
